8e91b0aae531bec3a7cbeff72bc07c67eede0bca
darpanradadiya <darpanradadiya576@gmail.com>
2022-03-05 23:21:18 +0530

    till 11.21

---
+++ arrays.js
@@ -0,0 +1,13 @@
+let arr = [1,2,3,5,];
+console.log(arr);
+
+let arr1 = [1,"sbha",4,5,6,true,'t',"trtr"]
+console.log(arr1);
+// arr1[0]=4.566;
+// arr1[3]="slmdksj";
+// console.log(arr1);
+// console.log(arr1[5]);
+arr1.push('i');
+console.log(arr1);
+arr1.pop();
+console.log(arr1);
