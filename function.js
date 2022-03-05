04372948b00af53d0257f7f5207ebc38a0dd5d86
darpanradadiya <darpanradadiya576@gmail.com>
2022-03-05 22:41:03 +0530

    store fn in variable

--- function.js
+++ function.js
@@ -6,6 +6,17 @@ function sum(a,b)
 {
     return(a+b);
 }
+function multiply(a,b,c)
+{
+    return(a*b*c);
+}
 sayhellu()
 let a=sum(20210,32020);
+let m=multiply(1,5,9);
 console.log(a);
+console.log(m);
+let k=function minus(num1,num2)
+{
+    return(num1-num2)
+}
+console.log(k(1048,8999));
