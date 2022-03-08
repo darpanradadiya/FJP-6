4a55d89451754d42e37ddf612bbe13dd658a9e75
darpanradadiya <darpanradadiya576@gmail.com>
2022-03-08 22:10:39 +0530

    till nested object

---
+++ object.js
@@ -0,0 +1,26 @@
+let obj = {};
+
+console.log(obj);
+
+let doreamon ={
+    name :"doriyo",
+    age:19,
+    height:1.5,
+    lastName :"Vadapav",
+    phoneNo : 6546545246
+
+
+}
+
+console.log(doreamon);
+let captain={
+    name : 'steve rogers',
+    age : '150',
+    friends : ['natasha romanhof','tony stark','hulk','captain carter'],
+    address : {
+        city : 'queens',
+        state : 'los angeles',
+    }
+
+}
+console.log(captain);
